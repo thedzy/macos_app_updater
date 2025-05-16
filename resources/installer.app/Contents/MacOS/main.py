ca240973dd35ed387f1d3cf659ce43cc68270ed1
Shane Young <shane.young@opentable.com>
2025-05-16 13:14:26 -0400

    more to come, will create an applicatiaon buiildier

---
+++ resources/installer.app/Contents/MacOS/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+""" 
+main.py
+
+Created by _yourname_
+Copyright (c) 2010 _comapnyname_, All Rights Reserved.
+"""
+
+import os
+import sys
+
+"""
+TODO
+	- Edit info.plist for copyright and organisation
+	- Replace cmd.icns
+	- Insert code
+"""
+
+
+def main():
+	# Get the path to the app location
+	basepath = os.path.abspath(__file__ + "/../../../..")
+
+	# Insert code and replce pass
+
+
+if __name__ == "__main__":
+	main()
